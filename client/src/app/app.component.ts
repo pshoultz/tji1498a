e7ee9badd78221b257757fbf7332bd609ff852c9
pshoultz <pshoultzula@gmail.com>
2021-07-11 22:35:17 -0500

    working on chrome cast to tv

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'client';
+    title: string = 'alpha build';
+    base: string = "/assets/";
+    ads: string[] = [this.base+'1.png', this.base+'2.jpg', this.base+'3.jpg'];
+    currentImage: string = "";
+    currIndex: number = 1;
+
+    public showAds(){
+        setTimeout(() => {
+            this.currentImage = this.ads[this.currIndex];
+
+            if(this.currIndex != this.ads.length - 1){
+                this.currIndex++;
+            }else{
+                this.currIndex = 0;
+            }
+
+            return this.showAds();
+        },1000)
+    }
 }
