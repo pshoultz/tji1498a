5a1b709d176656aac1ecf68a8d4bd22703f411ba
pshoultz <pshoultzula@gmail.com>
2021-12-05 17:01:29 -0600

    deleting because I cant figure it out

--- api/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/pshoultz/tji1498a/api/router"
-)
-
-func main() {
-	router.Start()
-}
--- api/router/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/pshoultz/tji1498a/api/router
-
-go 1.16
-
-require github.com/gin-gonic/gin v1.6.3
