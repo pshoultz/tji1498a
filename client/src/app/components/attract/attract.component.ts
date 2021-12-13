bd091464517601f5d78dfdd84c256c169c55c72d
pshoultz <pshoultzula@gmail.com>
2021-12-12 18:28:53 -0600

    redid structure

---
+++ client/src/app/components/attract/attract.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attract',
+  templateUrl: './attract.component.html',
+  styleUrls: ['./attract.component.css']
+})
+export class AttractComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
