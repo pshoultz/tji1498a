2bd3dd114265230d6d8e8e657609d29e14e66128
pshoultz <pshoultzula@gmail.com>
2022-01-23 23:33:04 -0600

    added models

--- api/db/db.go
+++ api/db/db.go
@@ -7,16 +7,19 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	//NOTE: project level imports
+	//User_Model "github.com/pshoultz/tji1498a/api/models/users"
 )
 
 var collection *mongo.Collection
 var ctx = context.TODO()
 
+//var user_model User_Model
+
 func Connect() {
 	//var user_struct User_struct
 	log.Println("trying to connect to db...")
 
-	//clientOptions := options.Client().ApplyURI("mongodb://mongodb:27017")
 	clientOptions := options.Client().ApplyURI("mongodb://127.0.0.1:27017")
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 
@@ -33,4 +36,14 @@ func Connect() {
 	fmt.Println("Connected to MongoDB!")
 
 	collection = client.Database("src16-beta").Collection("users")
+}
+
+func Disconnect(m *mongo.Client) bool {
+	err := m.Disconnect()
+
+	if err != nil {
+		return false
+	}
+
+	return true
 }
---
+++ api/models/users.go
@@ -0,0 +1,6 @@
+package models
+
+type User_Model struct {
+	Id    string `json:"id,omitempty" bson:"id,omitempty"`
+	Email string `json:"email,omitempty" bson:"email,omitempty"`
+}
